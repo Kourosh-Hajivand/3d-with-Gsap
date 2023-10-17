23927f675754e3e6c344478f745e28379ec888a0
kourosh-H <kourosh.hajivand0@gmail.com>
2023-10-17 19:23:58 +0330

    Add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
-import { useMemo, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-  const test = () => {
-    return Math.random() * 1000;
-  };
-  const memo = useMemo(() => test(), []);
-  console.log("====================================");
-  console.log(memo);
-  console.log("====================================");
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
